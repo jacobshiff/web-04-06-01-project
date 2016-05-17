b044d240b30987bdf7a8a34c38ec9fea758c0cff
rnbronstein <rnbronstein@gmail.com>
2016-05-17 18:16:11 -0400

    fill models and migrations for users, groups, memberships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :memberships
+  has_many :groups, through: :memberships
+  has_many :likes
+  has_many :comments
+  has_many :memes, foreign_key: 'creator_id'
+  has_many :invites, foreign_key: 'inviter_id'
+  has_many :invites, foreign_key: 'invitee_id'
 end
--- db/migrate/20160517220134_create_groups.rb
+++ db/migrate/20160517220134_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
-
+      t.string :title,
+      t.integer :group_creator_id,
       t.timestamps null: false
     end
   end
