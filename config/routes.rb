30e7e495f318e8575edee1ac2649f14a96f993e1
rnbronstein <rnbronstein@gmail.com>
2016-05-18 17:42:02 -0400

    literally just added one route alias

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   #   resources :memes, only: [:index, :show]
   # end
 
-  get '/:group_slug/memes' => 'memes#index'
+  get '/:group_slug/memes' => 'memes#index', as: 'memes'
   get '/:group_slug/memes/:id' => 'memes#show', as: 'meme'
 
   # This needs to go last
