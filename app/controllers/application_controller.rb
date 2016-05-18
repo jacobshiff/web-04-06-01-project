a104e60f3b24704492f41943bd61a347a4d2d406
rnbronstein <rnbronstein@gmail.com>
2016-05-18 19:48:16 -0400

    skeleton navbar changes based on logged in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def logged_in?
+    session[:user_id] != nil
+  end
+
+  def require_login
+    unless session[:user_id]
+      redirect_to signin_path
+    end
+  end
+
+  def set_user
+    if logged_in?
+      @user = User.find(session[:user_id])
+    end
+  end
+
 end
