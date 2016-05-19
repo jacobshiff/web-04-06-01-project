b6d19ec0dfadd3914fc80868b06c2cb9afe3f4d9
xristocodos <xristojobos@gmail.com>
2016-05-19 16:14:35 -0400

    eff branches we the baddest...jk sorry

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   #   resources :memes, only: [:index, :show]
   # end
 
-# Memes routes
+######### Memes routes
   #Create
   get '/:group_slug/memes/new' => 'memes#new'
   post '/:group_slug/memes' => 'memes#create'
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   #Destroy
   delete '/:group_slug/memes/:id' => 'memes#destroy'
 
-# Users
+######## Users
   # Login
   get '/:group_slug/login' => 'sessions#new', as: 'login'
   # post '/:group_slug/login' => 'sessions#create', as: 'login'
@@ -26,5 +26,13 @@ Rails.application.routes.draw do
   # Registration
   get '/:group_slug/users/new' => 'registrations#new', as: 'registration_new'
   post '/:group_slug/users/new' => 'registrations#create', as: 'registration_create'
+
+  # User path
+  get '/:group_slug/users/:username' => 'users#show', as: 'user'
+  get '/:group_slug/users/:username/edit' => 'users#edit', as: 'edit_user'
+  patch '/:group_slug/users/:username' => 'users#update'
+  delete '/:group_slug/users/:username' => 'users#destroy'
+
+  get '/profile' => 'users#show', as: 'user'
 
 end
