652d78e6021660fbbf24a0a8627687d1a6378664
jacobshiff <jacobshiff@gmail.com>
2016-05-20 17:52:58 -0400

    WIP

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
+  # config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['s3_access_key_id'],                        # required
     aws_secret_access_key: ENV['s3_secret_access_key'],                        # required
-    region:                'US Standard',                  # optional, defaults to 'us-east-1'
-    # host:                  's3.example.com',             # optional, defaults to nil
+    region:                'us-east-1',                  # optional, defaults to 'us-east-1'
+    host:                  's3.amazonaws.com',           # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'groupmeme'                          # required
