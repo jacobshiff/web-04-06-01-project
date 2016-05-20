f0841aa2db29c82047a67a510dd4fe2455df0190
rnbronstein <rnbronstein@gmail.com>
2016-05-20 14:41:14 -0400

    typo in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    binding.pry
-    @user = User.find(params[:user_id])
+    #binding.pry
+    @user = User.find(session[:user_id])
   end
 end
