d69984efb73046b91d17f7813e7511f8ca775a85
jacobshiff <jacobshiff@gmail.com>
2016-05-22 13:48:03 -0400

    WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+
+  def create
+  binding.pry
+  @comment = Comment.new(comment_params)
+  @comment.group = ____
+  @comment.user = ____
+  @comment.meme = _____
+    if @comment.save
+
+    end
+  end
+
+  private
+  def comment_params
+
+  end
+
 end
